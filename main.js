7b1b49f6fade8f86b4e69c9b5cf2430d57514e7c
Samin Ahmed <saminahmed1001@gmail.com>
2025-03-07 16:22:21 +0600

    feat: finish game logic

--- main.js
+++ main.js
@@ -31,8 +31,8 @@ const Gameboard = (function(){
 
 
 // Creates Player
-function player(name, marker, moves){
-    return {name, marker, moves}
+function player(name, marker, moves, color){
+    return {name, marker, moves, color}
 }
 
 
@@ -40,17 +40,15 @@ function player(name, marker, moves){
 // Game controller Module
 const controller = (function(){
 
-    let playerOneName = prompt("Enter the name of player one");
-    let playerTwoName = prompt("Enter the name of player Two");
-    while(playerOneName == playerTwoName){
-        playerTwoName = prompt("Both player can't have the same name, enter a different one!")
-    }
+    let playerOneName = "Samin";
+    let playerTwoName = "Pamin";
 
-    let playerOne = player(playerOneName, "X", [])
-    let playerTwo = player(playerTwoName, "O", [])
+    let playerOne = player(playerOneName, "X", [], "#EA3323")
+    let playerTwo = player(playerTwoName, "O", [], "#0000F5")
 
-    let round = 1;
-    let currentPlayer = round%2 == 0 ? playerTwo : playerOne;
+    
+
+    const boxes = document.querySelectorAll(".box");
 
     function checkWin(moves){
 
@@ -70,67 +68,59 @@ const controller = (function(){
     }
 
     function restart(){
-        let askRestart = prompt("Restart(Y/n)?")
-        if(askRestart == "Y" || askRestart == "y"){
-            console.clear();
-            Gameboard.resetBoard();
-            round++;
-            newGame();
-        }
+        const button = document.querySelector(".actionBtn");
+        button.style.display = "block";
+        button.addEventListener("click", () => {
+            location.reload();
+        })
     }
 
     function newGame(){
-        playerOne.moves = [];
-        playerTwo.moves = [];
+        let currentPlayer = playerOne;
         let emptyCell = 9;
-        while(emptyCell){
-            Gameboard.displayBoard();
-            let cell = prompt(`${currentPlayer.name}'s move[1-8]`);
-            while(!cell || isNaN(cell) || cell > 8 || playerOne.moves.includes(cell) || playerTwo.moves.includes(cell)){
-                cell = prompt(`Issue found, please re-enter`)
-            }
+        let resultDiv = document.querySelector(".resultDiv");
+        console.log(emptyCell);
+        boxes.forEach(box => {
+            
+            box.addEventListener("click", () => {
+                
+                let cell = box.id;
+                
+                // checks if cell is occupied or not
+                if(!box.querySelector("img") && emptyCell !== "filled"){
+                    const image = document.createElement("img");
+                    image.setAttribute("src", `icons/${currentPlayer.marker}.svg`);
+                    box.appendChild(image);
 
-            // Add move to current player's moves array
-            currentPlayer.moves.push(cell)
+                    // Add move to current player's moves array
+                    currentPlayer.moves.push(cell)
+                    Gameboard.updateCell(cell, currentPlayer.marker);
+                    Gameboard.displayBoard();
 
-            let marker = currentPlayer.marker;
-            Gameboard.updateCell(cell, marker);
+                    if(checkWin(currentPlayer.moves)){
+                        emptyCell = "filled";
+                        console.log(`${currentPlayer.name} is the winner!`)
+                        resultDiv.textContent = `${currentPlayer.name} wins!`;
 
-            if(checkWin(currentPlayer.moves)){
-                Gameboard.displayBoard();
-                console.log(`${currentPlayer.name} is the winner!`)
-                restart();
-                break;
-            }
-            else{
-                // Update current player
-                currentPlayer == playerOne ? currentPlayer = playerTwo : currentPlayer = playerOne;
-                emptyCell -= 1;
-            }
-            // Checks for draw
-            if(!emptyCell){
-                Gameboard.displayBoard();
-                console.log("Draw!")
-                restart()
-            }
-        }
-
+                        restart();
+                    }
+                    else{
+                        // Update current player
+                        currentPlayer = currentPlayer == playerOne ? playerTwo : playerOne;
+                        emptyCell -= 1;
+                        console.log(emptyCell);
+                    }
+                    // Checks for draw
+                    if(emptyCell <= 0){
+                        console.log("Draw!");
+                        resultDiv.textContent = "Draw";
+                        restart();
+                    }
+                }
+            })
+        })
     }
     newGame()
 })()
-
-
-
-
-
-// const button = document.querySelector(".actionBtn");
-// button.addEventListener("click", (event) => {
-//     if(event.target.innerText == "Start"){
-//         button.textContent = "End";
-//     }
-//     else if(event.target.innerText == "End"){
-//         button.textContent = "Start"
-//     }
-// })
 
 
